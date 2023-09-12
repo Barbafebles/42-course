28b7d0d363cebc27a947ea6d28f65df2dccbe60e
barbafebles <barbafebleswork@skiff.com>
2023-09-12 12:11:18 +0200

    update

--- printf/main.c
+++ printf/main.c
@@ -4,8 +4,8 @@ int main(void)
 {
   int   i;
   int   j;
-  char  *s = "hola lucia";
+  char  *s = "hola esto es una prueba";
   i = ft_printf("Holaa  %c %s %p %x %X %d %d %u %%\n", 'A', s, s, 255, 255, 250, 343, -343);
   j = printf("Holaa  %c %s %p %x %X %d %d %u %%\n", 'A', s, s, 255, 255, 250, 343, -343);
 	ft_printf("mio:\t\t%i\noriginal:\t%i", i, j);
-	}
+}
