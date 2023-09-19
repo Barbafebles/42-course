87f83aad2a846925253c0ca10ab5e9790e2e50f0
barbafebles <barbafebleswork@skiff.com>
2023-09-19 15:23:04 +0200

    update

--- push_swap/check_error.c
+++ push_swap/check_error.c
@@ -6,13 +6,13 @@
 /*   By: barbarafebles <barbarafebles@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 17:48:42 by barbarafebl       #+#    #+#             */
-/*   Updated: 2023/09/15 20:00:59 by barbarafebl      ###   ########.fr       */
+/*   Updated: 2023/09/17 15:06:06 by barbarafebl      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int check_error(char *str)
+int ft_isnumber(char *str)
 {
     int i; 
 
@@ -32,47 +32,93 @@ int check_error(char *str)
     }
     return 1;
 }
-int	ft_strcmp(char *s1, char*s2)
-{
-	unsigned int	m;
 
-	m = 0;
-	while (s1[m] == s2[m] && s1[m] != '\0' && s2[m] != '\0')
+int ft_strcmp(char *str1, char *str2)
+{
+	int i;
+	i = 0;
+	if (!str1 || !str2)
+		return 0;
+	
+	while(str1[i] != '\0' && str2[i] != '\0')
 	{
-			m++;
+		if (str1[i] == str2[i])
+			i++;
+		
+		else
+			return 0;
 	}
-	return (s1[m] - s2[m]);
+	return 1;
+}
+
+int valid_int(const char *str)
+{
+	int signo;
+	int i;	
+	
+	signo = 1;
+	i = 0;
+
+	if(str[i] == '\0')
+		return 0;
+
+	if((str[i] = '-') || (str[i] = '+'))
+		if(str[i] = '-')
+		signo = -1;
+		i++;;
+	
+	long long number;
+
+	while(str[i] != '\0')
+	{
+		if(str[i] < 0 || str[i] > 9)
+			return 0;
+		int number = number * 10 (str[i] - '0');
+	if(number * signo > INT_MAX || number * signo < INT_MIN)
+		return 0;
+	i++;
+	}
+	return 1;
 }
 
 /*
-int ft_strcmp(char *str1, char *str2)
-{
-   int i;
-	
-   if(!str1 || !str2)
-	return 0;
-
-   while(str1 != '\0' ||  str2 != '\0')
-	{
-		if(str1[i] == str2[i])
-			i++;
-		else 
-			return 0;
-	}
-	return 1;	
-}
-*/
-
-
 int main(int argc, char **argv)
 {
     int	j = 1;
 
     while(j+1 < argc)
     {
-	printf("%d", ft_strcmp(argv[j], argv[j+1]));
+	printf("%d", valid_int(argv[j], argv[j+1]));
         //printf("%d", check_error(argv[j]));
             j++;
     }
     return(0);
 }
+*/
+
+int main(char argc, char *argv)
+{
+	int i = 1;
+
+    	if (argc != 2) 
+	{
+        	printf("Uso: %d <cadena>\n", argv[0]);
+        	return 1;
+    	}
+	
+	const char *cadena;
+	cadena = argv[i];
+	
+	if(valid_int(cadena))
+	{
+		int numero = atoi(cadena);
+		printf("El numero entero es: %d\n", numero);
+	}
+	else 
+	{
+		printf("La cadena no es un valor valido de int.\n");
+	}
+	return 0;
+}
+
+
