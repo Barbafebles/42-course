b371aadbf6d335d4e5310b1083ad12a64f57cd90
barbafebles <barbafebles@gmail.com>
2024-04-02 13:54:14 +0200

    update

---
+++ pipex/help.c
@@ -0,0 +1,24 @@
+#include "pipex.h"
+
+void    execute(char *cmd, int in, int out)
+{
+    pid_t pid;
+    pid = fork();
+
+    if (pid == -1)
+    {
+        ft_error("Error en el fork");
+    }
+    else if (pid > 0) // padre 
+    {
+        // printf("El proceso hijo: PID %d\n", pid);
+    }
+    // redirecciono el stdin 
+    if (in != -1)
+    {
+        if (dup2(in, STDIN_FILENO) == -1)
+            ft_error("Error en redireccionar la stdin");
+    }
+    close (in); // cierra el fd que no necesito
+
+}
