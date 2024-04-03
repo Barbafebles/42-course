dea25a267a1ebad254ccb99216d1f7145e11e3a2
Barbara Febles Gomez <bfebles-@c2r1s1.42madrid.com>
2024-04-03 14:38:48 +0200

    update

--- pipex/help.c
+++ pipex/help.c
@@ -1,42 +1,57 @@
 #include "pipex.h"
 
-void    execute(char *cmd[], int in, int out)
+void execute(char *cmd[], int in, int out, char **envp) 
 {
     pid_t pid;
     pid = fork();
 
-    if (pid == -1)
+    if (pid == -1) 
     {
+        // Si falla el fork
         ft_error("Error en el fork");
-    }
-    else if (pid > 0) // padre 
+    } 
+    else if (pid == 0) // Proceso hijo
     {
-        // printf("El proceso hijo: PID %d\n", pid);
-    }
-    // redirecciono el stdin 
-    if (in != -1)
+        // Redirigir stdin si es necesario
+        if (in != -1) 
+        {
+            if (dup2(in, STDIN_FILENO) == -1)
+                ft_error("Error en redireccionar la stdin");
+            close(in); // Cierra el fd que no necesito
+        }
+        // Redirigir stdout si es necesario
+        if (out != -1) 
+        {
+            if(dup2(out, STDOUT_FILENO) == -1)
+                ft_error("Error en la redireccion de stdout");
+            close(out);
+        }
+        // Ejecutar el comando
+        if (execve(cmd[0], cmd, envp) == -1) 
+        {
+            ft_error("Error en execve");
+            exit(EXIT_FAILURE); // Salir si falla execve
+        }
+    } 
+    else 
     {
-        if (dup2(in, STDIN_FILENO) == -1)
-            ft_error("Error en redireccionar la stdin");
-        close (in); // cierra el fd que no necesito
-    }
-
-    if (out != -1)
-    {
-        if(dup2(out, STDOUT_FILENO) == -1)
-            ft_error("Error en la redireccion de stout");
-        close (out);
-    }
-
-    if (execvp(cmd[0], cmd) == -1)
-        ft_error("Error en el exceve");
-
-    else
-    {
+        // Esperar por el proceso hijo
         waitpid(pid, NULL, 0);
     }
 }
-int main (int argv, char *argv[])
+
+char **environ; 
+int main() 
 {
-    
+    // Comando a ejecutar: 'ls -l'
+    char *cmd[] = {"/bin/ls", "-l", NULL};
+
+    // No se requiere redirección, así que usamos -1 para 'in' y 'out'
+    int in = -1;
+    int out = -1;
+
+    // Llama a execute con el comando, sin redirecciones y el entorno actual
+    execute(cmd, in, out, environ);
+
+    return 0;
 }
