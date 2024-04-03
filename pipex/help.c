55b1b2bc65522ca2321fafb737ba84eff3f47eed
Barbara Febles Gomez <bfebles-@c2r1s3.42madrid.com>
2024-04-03 12:14:08 +0200

    update

--- pipex/help.c
+++ pipex/help.c
@@ -1,6 +1,6 @@
 #include "pipex.h"
 
-void    execute(char *cmd, int in, int out)
+void    execute(char *cmd[], int in, int out)
 {
     pid_t pid;
     pid = fork();
@@ -18,7 +18,25 @@ void    execute(char *cmd, int in, int out)
     {
         if (dup2(in, STDIN_FILENO) == -1)
             ft_error("Error en redireccionar la stdin");
+        close (in); // cierra el fd que no necesito
     }
-    close (in); // cierra el fd que no necesito
 
-}                                      
+    if (out != -1)
+    {
+        if(dup2(out, STDOUT_FILENO) == -1)
+            ft_error("Error en la redireccion de stout");
+        close (out);
+    }
+
+    if (execvp(cmd[0], cmd) == -1)
+        ft_error("Error en el exceve");
+
+    else
+    {
+        waitpid(pid, NULL, 0);
+    }
+}
+int main (int argv, char *argv[])
+{
+    
+}
