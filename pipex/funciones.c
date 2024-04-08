b4e15e0134259e341af7e20d7c95bb43a1edfe57
barbafebles <barbafebles@gmail.com>
2024-04-08 16:13:04 +0200

    update

--- pipex/funciones.c
+++ pipex/funciones.c
@@ -320,21 +320,298 @@ int main()
 /*
 dup2
 int dup2(int oldfd, int newfd);
-oldfd: el fd viejo 
+oldfd: el fd viejo
 newfd el fd nuevo que vas a copiar con dup2() 
+
+int main()
+{
+    int newfd;
+    int oldfd;
+
+    oldfd = open("datos.txt", O_RDONLY);
+    if (oldfd == -1)
+    {
+        ft_error("Error al abrir el fd");
+        return (1);
+    }
+    close (0);
+    newfd = dup2(oldfd, 0);
+    if (newfd == -1)
+    {
+        ft_error("Error duplicando el fd");
+    }
+    printf("El old: %d\n", oldfd);
+    printf("El nuevo %d\n", newfd);
+    close (oldfd);
+
+    return (0);
+}
 */
 
-int main() 
-{ 
-    int file_desc = open("datos.txt",O_WRONLY | O_APPEND); 
-      
-    // here the newfd is the file descriptor of stdout (i.e. 1) 
-    dup2(file_desc, 1) ;  
-          
-    // All the printf statements will be written in the file 
-    // "tricky.txt" 
-    printf("I will be printed in the file tricky.txt\n"); 
-      
-    return 0; 
- 
+//-------------------------------------------------------------------------------------------------------------------------------
+
+/*
+execve
+int execve(const char* path, const char* argv[], char* const envp[])
+
+
+void execute(char *cmd[], int in, int out, char **envp) 
+{
+    pid_t pid;
+    pid = fork();
+
+    if (pid == -1)
+    {
+        // Si falla el fork
+        ft_error("Error en el fork");
+    } 
+    else if (pid == 0) // Proceso hijo
+    {
+        // Redirigir stdin si es necesario
+        if (in != -1) 
+        {
+            if (dup2(in, STDIN_FILENO) == -1)
+                ft_error("Error en redireccionar la stdin");
+            close(in); // Cierra el fd que no necesito
+        }
+        // Redirigir stdout si es necesario
+        if (out != -1) 
+        {
+            if(dup2(out, STDOUT_FILENO) == -1)
+                ft_error("Error en la redireccion de stdout");
+            close(out);
+        }
+        // Ejecutar el comando
+        if (execve(cmd[0], cmd, envp) == -1) 
+        {
+            ft_error("Error en execve");
+            exit(EXIT_FAILURE); // Salir si falla execve
+        }
+    } 
+    else 
+    {
+        // Esperar por el proceso hijo
+        waitpid(pid, NULL, 0);
+    }
 }
+
+char **environ; 
+int main() 
+{
+    // Comando a ejecutar: 'ls -l'
+    char *cmd[] = {"/bin/ls", "-l", NULL};
+
+    // No se requiere redirección, así que usamos -1 para 'in' y 'out'
+    int in = -1;
+    int out = -1;
+
+    // Llama a execute con el comando, sin redirecciones y el entorno actual
+    execute(cmd, in, out, environ);
+
+    return 0;
+}
+
+*/
+
+//----------------------------------------------------------------------------------------------------------------------------------
+
+/*
+exit 
+void exit(int status);
+
+int main()
+{
+    int fd;
+    fd = open("datos.txt", O_RDONLY);
+    if (fd == -1)
+    {
+        ft_error("Error al abrir el fd");
+        exit (1);
+    }
+    else if (fd)
+    {
+        printf("El fd: %d\n", fd);
+    }
+    return (0);
+}
+*/
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+
+/*
+fork
+void forkexample()
+{
+    pid_t p;
+    p = fork();
+    if (p < 0)
+    {
+      perror("fork fail");
+      exit(1);
+    }
+    // proceso hijo por que devuelve 0
+    else if ( p == 0)
+        printf("Hello from Child!\n");
+ 
+    // proceso padre 
+    else
+        printf("Hello from Parent!\n");
+}
+int main()
+{
+    forkexample();
+    return 0;
+}
+*/
+
+//-----------------------------------------------------------------------------------------------------------------------
+
+/*
+pipe 
+int pipe(int fds[2]);
+
+Parameters :
+fd[0] will be the fd(file descriptor) for the 
+read end of pipe.
+fd[1] will be the fd for the write end of pipe.
+Returns : 0 on Success.
+-1 on error.
+
+int main()
+{
+    int pipefd[2]; // donde guardo los pipe fd [0]read [1]write
+    pid_t   pid; // donde guardo el fork value 
+    char    buf; // donde vamos a guardar los caracteres read by read 
+
+// creamos un pipe donde vamos a guardar los fd 
+    if (pipe(pipefd) == -1) // 
+    {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+
+// creamos el fork 
+    pid = fork();
+    if (pid == -1)
+    {
+        ft_error("Fork");
+    }
+    else if (pid == 0) // proceso hijo 
+    {
+        // cerrar el extremo del pipe de escritura
+        close(pipefd[1]);
+        writestr(STDOUT_FILENO, "Hijo: cual es le secreto en este pipe?\n");
+        writestr(STDOUT_FILENO, "Hijo: \"");
+        // Leer los caracteres del pipe uno por uno 
+        while(read(pipefd[0],&buf,1) > 0)
+        {
+            // Escribir el carácter leído en la salida estándar
+            write(STDOUT_FILENO, &buf, 1);
+        }
+        writestr(STDOUT_FILENO, "\"\n");
+        writestr(STDOUT_FILENO, "Hijo: Ohh wow tu eres mi padre \n");
+        // cerrar el proceso 
+        close(pipefd[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    else // proceso padre 
+    {
+        // cierro la lectura 
+        close(pipefd[0]);
+        writestr(STDERR_FILENO, "Padre: Estoy escribiendo un secreto en este pipe\n");
+        // escribimos dentro del pipe
+        writestr(pipefd[1], "\e[33mYo soy tu padre!\e[0m");
+        // Cierra el final de escritura de la tubería (el lector verá EOF)
+        close(pipefd[1]);
+        //esperamos al hijo 
+        wait(NULL);
+        writestr(STDOUT_FILENO, "Padre: hola hijo\n");
+        exit(EXIT_SUCCESS);
+    }
+
+}
+*/
+
+//------------------------------------------------------------------------------------------------------------------------------------
+
+/*
+unlink
+int unlink(const char *pathname)
+pathname = ruta del archivo que quiero eliminar 
+
+si tiene exito devuelve 0 sino devuelve -1 y errno establece el 
+
+me elimina el .txt
+
+int main()
+{
+    if (unlink("datos.txt") == -1)
+    {
+        ft_error("Error eliminando el archivo");
+    }
+    
+    printf("Archivo eliminado con exito\n");
+    return (0);
+}
+*/
+
+//--------------------------------------------------------------------------------------------------------------------------------------
+
+/*
+wait 
+
+
+int main()
+{
+    pid_t pid;
+    pid = fork();
+    if (pid == -1)
+    {
+        ft_error("Fallo en crear el fork");
+    }
+    if (pid == 0) // proceso hijo
+    {
+        wait(NULL);
+        printf("Soy el hijo\n", getpid());
+    }
+    if (pid > 0)
+    {
+        printf("Soy el proceso padre voy primero\n", getppid());
+    }
+    return (0);
+}
+*/
+
+//---------------------------------------------------------------------------------------------------------------------------------------
+
+/*
+waitpid
+pid_t waitpid(pid_t pid, int *status, int options);
+pid = ID del proceso 
+*status = variable entera donde wairpid almacena el estado de salida del padre 
+options = banderas que cambian con waitpid 
+
+#include <sys/wait.h> // Asegúrate de incluir esta biblioteca para usar waitpid()
+
+int main()
+{
+    pid_t pid;
+    pid = fork();
+    if (pid == -1)
+    {
+        ft_error("Fallo en crear el fork");
+    }
+    if (pid == 0) // proceso hijo
+    {
+        printf("Soy el hijo\n", getpid());
+    }
+    else // proceso padre
+    {
+        int status;
+        waitpid(pid, &status, 0); // El proceso padre espera al hijo especificado por pid
+        printf("Soy el proceso padre voy primero\n", getppid());
+    }
+    return (0);
+}
+*/
