a6ceab0ea908986ac3c4c0ce64d5c613e0fe5f99
barbafebles <barbafebles@gmail.com>
2024-04-15 21:02:35 +0200

    update

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -3,27 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: barbarafebles <barbarafebles@student.42    +#+  +:+       +#+        */
+/*   By: barbafebles <barbafebles@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 14:45:23 by barbafebles       #+#    #+#             */
-/*   Updated: 2024/04/15 13:58:06 by barbarafebl      ###   ########.fr       */
+/*   Updated: 2024/04/15 20:51:22 by barbafebles      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void parse(int argc, char *argv[], char ***cmd1, char ***cmd2, char **infile, char **outfile)
+#include "pipex.h"
+
+void parse(int argc, char *argv[], char **cmd1, char **cmd2, char **infile, char **outfile)
 {
+    // se comprueba que el numero de argumentos es igual a 5
     if (argc != 5)
     {
         ft_error("Numero incorrecto de argumentos");
     }
+    // se comprueba que argv[1], argv[2], argv[3] y argv[4] no son NULL antes de usarlos
+    if (argv[1] == NULL || argv[2] == NULL || argv[3] == NULL || argv[4] == NULL)
+    {
+        ft_error("Argumento(s) no proporcionado(s)");
+    }
+    // se separaan los argumentos que pasaran por el pipex
     *cmd1 = ft_split(argv[2], ' '); // argv[2] y argv[3]
     *cmd2 = ft_split(argv[3], ' '); 
-
+    // asignacion de los argumentos 1 y 4 (nombres de entrada y salida)
     *infile = argv[1];
     *outfile = argv[4];
-
+    // si cmd1 y cmd2 es igual a NULL devuelve error 
     if (*cmd1 == NULL || *cmd2 == NULL)
     {
         ft_error("Error al parseo de los argumentos");
@@ -45,18 +54,9 @@ int main(int argc, char *argv[])
     printf("Comando 1: %s\n", cmd1[0]); // Solo imprime el primer elemento para prueba
 }
 */
+//--------------------------------------------------------------------------------------------------------------------------
 
-
-/*
-    Crear una tubería con pipe.
-    Usar fork para crear un proceso hijo que ejecute el primer comando.
-    Configurar las redirecciones adecuadas con dup2.
-    Ejecutar el comando con execve.
-    Repetir un proceso similar para el segundo comando.
-*/
-
-
-void exec_comd1(int *pipefd, int infile_fd, char **cmd, char **envp)
+void exec_comd1(int *pipefd, char **cmd, char **envp)
 {
     pid_t pipe;
     pipe = fork();
@@ -82,3 +82,27 @@ void exec_comd1(int *pipefd, int infile_fd, char **cmd, char **envp)
         }
     }
 }
+
+void exec_cmd2(int *pipefd, char **cmd, char **envp)
+{
+    pid_t pipe;
+    pipe = fork();
+    if (pipe == -1)
+    {
+        ft_error("Error creando el fork");
+    }
+    if (pipe == 0)
+    {
+        if (dup2(pipefd[0], STDIN_FILENO) == -1)
+        {
+            ft_error("Error con dup2");
+        }
+        close(pipefd[1]);
+        close(pipefd[0]);
+        if (execve(cmd[0], cmd, envp) == -1)
+        {
+            ft_error("Error");
+        }
+    }
+}
+
