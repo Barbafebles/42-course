02295934f1f4905235dc33014cd6f7e7449b3e7e
Barbara Febles Gomez <bfebles-@c1r9s3.42madrid.com>
2024-10-19 08:52:57 +0200

    casi final

--- minitalk final/minitalk.h
+++ minitalk final/minitalk.h
@@ -1,8 +1,8 @@
 #ifndef MINITALK_H
 # define MINITALK_H
 
-# include "/Users/barbarafebles/Desktop/42/42-course/minitalk final/ft_printf/ft_printf.h"
-# include "/Users/barbarafebles/Desktop/42/42-course/minitalk final/libft/libft.h"
+# include "../ft_printf/ft_printf.h"
+# include "../libft/libft.h"
 # include <signal.h>
 # include <stdlib.h>
 # include <unistd.h>
